db603fdd82435fe508a52cbc546b5de7872abc5c
Ashu Baweja <ashu.baweja@jublfood.com>
2020-08-19 12:41:17 +0530

    Added Gallery VC

---
+++ Skuad/Skuad/Controllers/GalleryVC.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryVC.swift
+//  Skuad
+//
+//  Created by Ashu Baweja on 19/08/20.
+//  Copyright © 2020 iOS Developer. All rights reserved.
+//
+
+import UIKit
+
+class GalleryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
