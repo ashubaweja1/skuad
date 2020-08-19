9b0c53a0dad6248f52fb8781804748f47cf14baa
Ashu Baweja <ashu.baweja@jublfood.com>
2020-08-19 12:56:26 +0530

    Added images table view on Search VC

--- Skuad/Skuad/Controllers/SearchVC.swift
+++ Skuad/Skuad/Controllers/SearchVC.swift
@@ -8,10 +8,48 @@
 
 import UIKit
 
+// MARK: Private Constants
+private let kImageListCell = "ImageListCell"
+
+
 class SearchVC: UIViewController {
 
+    @IBOutlet weak var imagesTableView: UITableView!
+    
+    // MARK: Variables
+    var imageList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let imageListCell = UINib(nibName: kImageListCell, bundle: nil)
+        imagesTableView.register(imageListCell, forCellReuseIdentifier: kImageListCell)
     }
 }
+
+
+// MARK: UITableView Delegates & Datasource
+extension SearchVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageList.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kImageListCell, for: indexPath)
+        cell.selectionStyle = .none
+        if let imageListCell = cell as? ImageListCell {
+            print("configure cell")
+        }
+        return cell
+    }
+}
+
---
+++ Skuad/Skuad/CustomCells/ImageListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageListCell.swift
+//  Skuad
+//
+//  Created by Ashu Baweja on 19/08/20.
+//  Copyright © 2020 iOS Developer. All rights reserved.
+//
+
+import UIKit
+
+class ImageListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
