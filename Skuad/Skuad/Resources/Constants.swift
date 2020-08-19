dc000afe00fad606901e7e509fa2ec76c16625b5
Ashu Baweja <ashu.baweja@jublfood.com>
2020-08-19 13:45:41 +0530

    Created Constants File

---
+++ Skuad/Skuad/Resources/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  Skuad
+//
+//  Created by Ashu Baweja on 19/08/20.
+//  Copyright © 2020 iOS Developer. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Api Related Constants
+let kBaseUrl = "https://pixabay.com/api/"
+let kApiKey = "17960085-fb2b35c842eb0c594211577c4"
+    
+let kKey = "key"
+let kQuery = "q"
+let kImageType = "image_type"
+let kPhoto = "photo"
+let kPage = "page"
+
+let kTotalHits = "totalHits"
+let kHits = "hits"
+let kId = "id"
+let kDownloads = "downloads"
+let kLikes = "likes"
+let kFavorites = "favorites"
+let kUserImageURL = "userImageURL"
