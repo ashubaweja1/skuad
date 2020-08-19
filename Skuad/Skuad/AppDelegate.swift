07caa44384b092941dea3af093edd51516595fea
Ashu Baweja <ashubaweja15@gmail.com>
2020-08-19 12:31:17 +0530

    Project Created

---
+++ Skuad/Skuad/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Skuad
+//
+//  Created by Ashu Baweja on 19/08/20.
+//  Copyright © 2020 iOS Developer. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ Skuad/Skuad/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Skuad
+//
+//  Created by Ashu Baweja on 19/08/20.
+//  Copyright © 2020 iOS Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
