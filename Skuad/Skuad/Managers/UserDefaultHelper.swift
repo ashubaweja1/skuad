be8a31e5a8ac22a83044473988d0db485ebdbed0
Ashu Baweja <ashu.baweja@jublfood.com>
2020-08-20 10:52:42 +0530

    Created User Defaults Helper class

---
+++ Skuad/Skuad/Managers/UserDefaultHelper.swift
@@ -0,0 +1,53 @@
+//
+//  UserDefaultHelper.swift
+//  Skuad
+//
+//  Created by Ashu Baweja on 20/08/20.
+//  Copyright © 2020 iOS Developer. All rights reserved.
+//
+
+import Foundation
+
+private let kSuggestionList = "SuggestionList"
+
+class UserDefaultHelper {
+    
+    class func set(_ value : Any?, forkey key : String) {
+        if let value = value {
+            UserDefaults.standard.set(value, forKey: key)
+        } else {
+            UserDefaults.standard.removeObject(forKey: key)
+        }
+        UserDefaults.standard.synchronize()
+    }
+    
+    class func get(_ key : String) -> Any? {
+        return UserDefaults.standard.object(forKey: key)
+    }
+    
+    class func addSuggestion(suggestion: String) {
+        var suggestions = [String]()
+        if let suggestionList = UserDefaultHelper.get(kSuggestionList) as? [String] {
+            suggestions = suggestionList
+            
+            if let index = suggestionList.firstIndex(of: suggestion) {
+                suggestions.remove(at: index)
+            }
+            
+            if suggestions.count == 10 {
+                suggestions.removeLast()
+            }
+        }
+        
+        suggestions.insert(suggestion, at: 0)
+        UserDefaultHelper.set(suggestions, forkey: kSuggestionList)
+    }
+    
+    class func getSuggestionList(suggestion: String) -> [String] {
+        var suggestions = [String]()
+        if let suggestionList = UserDefaultHelper.get(kSuggestionList) as? [String] {
+            suggestions = suggestionList
+        }
+        return suggestions
+    }
+}
